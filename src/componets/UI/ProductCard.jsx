60ab7f68b81977fc830476ab8eddc6db534a6db8
Emmanuel Otieno <emmanuelokello294@gmail.com>
2023-01-31 19:38:26 +0300

    Offers

--- src/componets/UI/ProductCard.jsx
+++ src/componets/UI/ProductCard.jsx
@@ -7,14 +7,14 @@ import { Link } from 'react-router-dom'
 
 export const ProductCard = ({item}) => {
   return (
-    <Col lg='3' md='4' >
+    <Col lg='3' md='4' className='mb-2'>
          <div className="product_item">
             <div className="product_img">
                 <motion.img whileHover={{scale: 0.9}}src={item.imgUrl} alt="" />
             </div>
          <div className='p-2 product_info'>
             <h3 className="product_name">
-                <Link to="/shop/id">{item.productName}</Link>
+                <Link to="/shop/${item.id}">{item.productName}</Link>
             </h3>
             <span className='text-center'>{item.category}</span>
          </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,17 +8,29 @@ import { motion } from "framer-motion"
 import Services from '../services/Services'
 import ProductList from '../componets/UI/ProductList'
 import products from '../assets/data/products'
+import counterImg from '../assets/images/counter-timer-img.png'
+import Clock from '../componets/UI/Clock'
 
 
 const Home = () => {
- const [data, setData] = useState(products)
+ const [trendingProducts, setTrendingProducts] = useState([])
+ const [bestSalesProducts, setBestSalesProducts] = useState([])
+
+
  const year = new Date().getFullYear()
  
  useEffect(()=>{
-  const filteredProducts = products.filter(
+  const filteredTrendingProducts = products.filter(
     item=>item.category === 'chair')
-    setData(filteredProducts)
+  
+  const filteredBestSalesProducts = products.filter(
+      item=>item.category === 'sofa')  
+
+    setTrendingProducts(filteredTrendingProducts)
+    setBestSalesProducts(filteredBestSalesProducts)
  }, []) 
+
+
   return <Helmet title ={'Home'}>
     <section className='hero_section'>
       <Container>
@@ -46,7 +58,35 @@ const Home = () => {
           <Col lg='12' className='text-center'>
             <h2 className='section_title'>Trending Products</h2>
           </Col>
-          <ProductList data={data}/>
+          <ProductList data={trendingProducts}/>
+        </Row>
+      </Container>
+    </section>
+    <section className="best_sales">
+      <Container>
+        <Row>
+          <Col lg="12" className='text-center'>
+            <h2 className="section_title">Best sales</h2>
+          </Col>
+          <ProductList data={bestSalesProducts}/>
+        </Row>
+      </Container>
+    </section>
+
+    <section className='timer_count'>
+      <Container>
+        <Row>
+          <Col lg='6' md='6'>
+            <div className="clock_top-content">
+              <h4 className='text-white fs-9 mb-3'>Limited Time Offer</h4>
+              <h3 className='text-white fs-6 mb-2'>Quality Armchair</h3>
+            </div>
+            <Clock />
+            <motion.button whileTap={{scale: 1.2}} className='buy_btn store_btn'><Link to='/shop'>Visit Shop</Link></motion.button>
+          </Col>
+          <Col lg='6' md='6' className='text-end'>
+            <img src={counterImg} alt="" />
+          </Col>
         </Row>
       </Container>
     </section>
